8cec352907a9a1a202bc7872412fd48f93ae6ef1
Vlad Savchenko <polkamaraka@gmail.com>
2020-04-30 13:34:29 +0300

    Initial commit

---
+++ ImageApp/app/src/main/java/it_school/sumdu/edu/imageapp/MainActivity.java
@@ -0,0 +1,32 @@
+package it_school.sumdu.edu.imageapp;
+
+import android.net.Uri;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
+public class MainActivity extends AppCompatActivity {
+    VideoView videoPlayer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        videoPlayer = (VideoView)findViewById(R.id.videoPlayer);
+        Uri myVideoUri= Uri.parse(R.raw.gu);
+        videoPlayer.setVideoURI(myVideoUri);
+        MediaController mediaController = new MediaController(this);
+        videoPlayer.setMediaController(mediaController);
+        mediaController.setMediaPlayer(videoPlayer);
+    }
+    public void play(View view){
+        videoPlayer.start();
+    }
+    public void pause(View view){
+        videoPlayer.pause();
+    }
+    public void stop(View view){
+        videoPlayer.stopPlayback();
+        videoPlayer.resume();
+    }
+}
